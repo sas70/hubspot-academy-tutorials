53735129e5e01659ca75fc84ade9cc31198e1125
Omar El Mghari <77205113+sas70@users.noreply.github.com>
2023-03-02 14:02:51 -0500

    update

--- hello-hubspot/01-building-a-hello-world-app/getCompanies.js
+++ hello-hubspot/01-building-a-hello-world-app/getCompanies.js
@@ -13,32 +13,31 @@ console.log("==========================================");
 url =
   "https://api.hubapi.com/crm/v3/objects/companies?limit=10&properties=domain%2Cname%2Cstate&archived=false";
 
-const allCompanies = hubspotClient.crm.companies
-  .getAll()
-  .then((results) => {
-    console.log(results);
-    console.log(
-      "There are " + results.length + " companies in your HubSpot account"
-    );
-  })
-  .catch((err) => {
-    console.error(err.message);
+async function getCompanies() {
+  const allCompanies = hubspotClient.crm.companies
+    .getAll()
+    .then((results) => {
+      saveComapnies(results);
+      console.log(results);
+      console.log(
+        "There are " + results.length + " companies in your HubSpot account"
+      );
+    })
+    .catch((err) => {
+      console.error(err.message);
+    });
+}
+
+getCompanies();
+
+// Function to save companies in a JSON file
+async function saveComapnies(companies) {
+  const fs = require("fs");
+  fs.writeFile("simple_companies.json", JSON.stringify(companies), (err) => {
+    if (err) {
+      console.log("Error writing file", err);
+    } else {
+      console.log("Successfully wrote file");
+    }
   });
-
-// new code
-
-//   const hubspot = require('@hubspot/api-client');
-
-// const hubspotClient = new hubspot.Client({"accessToken":"YOUR_ACCESS_TOKEN"});
-
-// const objectType = "objectType";
-// const archived = false;
-
-// try {
-//   const apiResponse = await hubspotClient.crm.properties.coreApi.getAll(objectType, archived);
-//   console.log(JSON.stringify(apiResponse, null, 2));
-// } catch (e) {
-//   e.message === 'HTTP request failed'
-//     ? console.error(JSON.stringify(e.response, null, 2))
-//     : console.error(e)
-// }
+}
--- hello-hubspot/01-building-a-hello-world-app/getOneCompProps.js
+++ hello-hubspot/01-building-a-hello-world-app/getOneCompProps.js
@@ -1,22 +1,20 @@
 // Description: This script gets the properties of a company from HubSpot and saves them to Firestore
 
+//Initialize the Firebase Admin SDK
+const admin = require("firebase-admin");
+const serviceAccount = require("./serviceAccountKey.json");
+admin.initializeApp({
+  credential: admin.credential.cert(serviceAccount),
+});
+firtestoreCollection = "hubSpot_companies";
+
+// import the required modules
 const fetch = require("node-fetch");
 require("dotenv").config();
+const fs = require("fs");
 const ACCESS_TOKEN = process.env.PRIVATE_APP_ACCESS_PROD;
 
-// read the properties from the json file and create a list of properties to pass to the API
-const fs = require("fs");
-const properties = JSON.parse(
-  fs.readFileSync("companiesPropertiesNameLabel.json", "utf8")
-);
-const properties_list = properties.map((property) => property.name).join(",");
-
-// input the company ID
-const companyId = "10060298063";
-
-// create the url & options
-const url = `https://api.hubapi.com/crm/v3/objects/companies/${companyId}?limit=10&properties=${properties_list}`;
-
+// create the options
 const options = {
   method: "GET",
   headers: {
@@ -25,42 +23,75 @@ const options = {
   },
 };
 
-let responseData;
-
-// start the async function to get the properties from HubSpot
+// ----------------------- Main ----------------------------
+// 1- get the lists of properties & companies
 (async () => {
-  const response = await fetch(url, options);
-  if (!response.ok) {
-    throw new Error(
-      `Hey, Your Request failed: ${response.status} ${response.statusText}`
-    );
+  const { properties_list, companies_list } = await getLists();
+
+  // 2- Iterate  over the companies list and get the properties for each company
+
+  for (let i = 0; i < companies_list.length; i++) {
+    let companyId = companies_list[i];
+    // start the async function to get the properties from HubSpot for one company at a time
+    (async () => {
+      const url = `https://api.hubapi.com/crm/v3/objects/companies/${companyId}?limit=10&properties=${properties_list}`;
+
+      let responseData;
+
+      const response = await fetch(url, options);
+      if (!response.ok) {
+        throw new Error(
+          `Hey, Your Request failed: ${response.status} ${response.statusText}`
+        );
+      }
+      responseData = await response.json();
+      if (responseData.status === "error") {
+        throw new Error(responseData.message);
+      }
+
+      let properties_count = Object.keys(responseData.properties).length;
+      let hs_companyId = responseData.id;
+      if (hs_companyId !== companyId) {
+        throw new Error(
+          `Hey, the company ID retrieved from HubSpot ${hs_companyId} does not match the provided company ID ${companyId}`
+        );
+      }
+
+      console.log(
+        `Great! it worked and there are ${properties_count} properties for this company named ${responseData.properties.name}`
+      );
+
+      // delete all null properties & those that start with "hs_analytics"
+      nonNull_properties = await deleteNullProperties(companyId, responseData);
+
+      // save the properties to Firestore
+      await saveToFirestore(nonNull_properties);
+    })();
   }
-  responseData = await response.json();
-  if (responseData.status === "error") {
-    throw new Error(responseData.message);
-  }
-
-  let properties_count = Object.keys(responseData.properties).length;
-  let hs_companyId = responseData.id;
-  if (hs_companyId !== companyId) {
-    throw new Error(
-      `Hey, the company ID retrieved from HubSpot ${hs_companyId} does not match the provided company ID ${companyId}`
-    );
-  }
-
-  console.log(
-    `Great! it worked and there are ${properties_count} properties for this company named ${responseData.properties.name}`
-  );
-
-  // delete all null properties & those that start with "hs_analytics"
-  nonNull_properties = await deleteNullProperties(companyId);
-
-  // save the properties to Firestore
-  await saveToFirestore(nonNull_properties);
 })();
 
+// ----------------------- Functions ----------------------------
+
+// Function to process the 2 json files & return the 2 lists of properties & companies
+async function getLists() {
+  // read the properties from the json file and create a list of properties to pass to the API
+  const properties = JSON.parse(
+    fs.readFileSync("companiesPropertiesNameLabel.json", "utf8")
+  );
+  const properties_list = properties.map((property) => property.name).join(",");
+
+  // Get the company ID from the json file "simple_companies.json" and save it to a list
+  const companies = JSON.parse(
+    fs.readFileSync("simple_companies.json", "utf8")
+  );
+  const companies_list = companies.map((company) => company.id);
+  console.log("size of the companies_list is  --- ", companies_list.length);
+
+  return { properties_list, companies_list };
+}
+
 // function to delete all null properties & those that start with "hs_analytics"
-async function deleteNullProperties(companyId) {
+async function deleteNullProperties(companyId, responseData) {
   let nonNull_properties = responseData.properties;
   for (let key in responseData.properties) {
     if (
@@ -75,19 +106,19 @@ async function deleteNullProperties(companyId) {
   console.log(
     `There are ${properties_count} non-null properties for this company named ${nonNull_properties.name}`
   );
-  console.log(nonNull_properties);
   return nonNull_properties;
 }
 
 // Function to save the properties to Firestore
 async function saveToFirestore(nonNull_properties) {
-  const admin = require("firebase-admin");
-  const serviceAccount = require("./serviceAccountKey.json");
-  admin.initializeApp({
-    credential: admin.credential.cert(serviceAccount),
-  });
-  let docID = nonNull_properties.name + "_" + nonNull_properties.companyId;
+  let companyName = nonNull_properties.name;
+  // replace the spaces with "_"
+  companyName = companyName.replace(/\s/g, "_");
+  // Keep only the first 2 words
+  companyName = companyName.split("_").slice(0, 2).join("_");
+
+  let docID = companyName + "_" + nonNull_properties.companyId;
   const db = admin.firestore();
-  const docRef = db.collection("hubSpot_companies").doc(`${docID}`);
+  const docRef = db.collection(firtestoreCollection).doc(`${docID}`);
   docRef.set(nonNull_properties);
 }
