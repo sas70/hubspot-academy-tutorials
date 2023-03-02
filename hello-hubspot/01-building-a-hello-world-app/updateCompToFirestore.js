6e6304844175a5f660f045d85f7791502925c72e
Omar El Mghari <77205113+sas70@users.noreply.github.com>
2023-03-02 16:52:04 -0500

    update

--- hello-hubspot/01-building-a-hello-world-app/updateCompToFirestore.js
+++ hello-hubspot/01-building-a-hello-world-app/updateCompToFirestore.js
@@ -1,120 +1,52 @@
 // Description: This script updates some properties of one company in HubSpot
 
-//Initialize the Firebase Admin SDK
-const admin = require("firebase-admin");
-const serviceAccount = require("./serviceAccountKey.json");
-admin.initializeApp({
-  credential: admin.credential.cert(serviceAccount),
-});
-firtestoreCollection = "hubSpot_companies";
-
 // import the required modules
 const fetch = require("node-fetch");
 require("dotenv").config();
-const fs = require("fs");
-const ACCESS_TOKEN = process.env.PRIVATE_APP_ACCESS_DEC;
+const ACCESS_TOKEN = process.env.PRIVATE_APP_ACCESS_DEV;
 
 // create the options
+properties_dict = {
+  properties: { name: "GreatNewName", domain: "GreatNewDomain.com" },
+};
+
 const options = {
   method: "PATCH",
   headers: {
     "Content-Type": "application/json",
     Authorization: `Bearer ${ACCESS_TOKEN}`,
   },
+  body: JSON.stringify(properties_dict),
 };
 
 // ----------------------- Main ----------------------------
-let companyId = "xxxxxxx";
-properties_dict = {
-  name: "newName",
-  state: "XY",
-}(
-  // start the async function to get the properties from HubSpot for one company at a time
-  async () => {
-    const url = `https://api.hubapi.com/crm/v3/objects/companies/${companyId}?limit=10&properties=${properties_dict}`;
+let companyId = "14917258266";
 
-    let responseData;
+// start the async function to get the properties from HubSpot for one company at a time
+(async () => {
+  const url = `https://api.hubapi.com/crm/v3/objects/companies/${companyId}`;
+  console.log(`url: ${url}) `);
 
-    const response = await fetch(url, options);
-    if (!response.ok) {
-      throw new Error(
-        `Hey, Your Request failed: ${response.status} ${response.statusText}`
-      );
-    }
-    responseData = await response.json();
-    if (responseData.status === "error") {
-      throw new Error(responseData.message);
-    }
+  let responseData;
 
-    let properties_count = Object.keys(responseData.properties).length;
-    let hs_companyId = responseData.id;
-    if (hs_companyId !== companyId) {
-      throw new Error(
-        `Hey, the company ID retrieved from HubSpot ${hs_companyId} does not match the provided company ID ${companyId}`
-      );
-    }
-
-    console.log(
-      `Great! it worked and there are ${properties_count} properties for this company named ${responseData.properties.name}`
+  const response = await fetch(url, options);
+  if (!response.ok) {
+    throw new Error(
+      `Hey, Your Request failed: ${response.status} ${response.statusText}`
     );
-
-    // delete all null properties & those that start with "hs_analytics"
-    nonNull_properties = await deleteNullProperties(companyId, responseData);
-
-    // save the properties to Firestore
-    await saveToFirestore(nonNull_properties);
   }
-)();
-
-// ----------------------- Functions ----------------------------
-
-// Function to process the 2 json files & return the 2 lists of properties & companies
-async function getLists() {
-  // read the properties from the json file and create a list of properties to pass to the API
-  const properties = JSON.parse(
-    fs.readFileSync("companiesPropertiesNameLabel.json", "utf8")
-  );
-  const properties_list = properties.map((property) => property.name).join(",");
-
-  // Get the company ID from the json file "simple_companies.json" and save it to a list
-  const companies = JSON.parse(
-    fs.readFileSync("simple_companies.json", "utf8")
-  );
-  const companies_list = companies.map((company) => company.id);
-  console.log("size of the companies_list is  --- ", companies_list.length);
-
-  return { properties_list, companies_list };
-}
-
-// function to delete all null properties & those that start with "hs_analytics"
-async function deleteNullProperties(companyId, responseData) {
-  let nonNull_properties = responseData.properties;
-  for (let key in responseData.properties) {
-    if (
-      responseData.properties[key] === null ||
-      key.startsWith("hs_analytics")
-    ) {
-      delete nonNull_properties[key];
-    }
+  responseData = await response.json();
+  if (responseData.status === "error") {
+    throw new Error(responseData.message);
   }
-  nonNull_properties["companyId"] = companyId;
-  properties_count = Object.keys(nonNull_properties).length;
+
   console.log(
-    `There are ${properties_count} non-null properties for this company named ${nonNull_properties.name}`
+    `Great! it worked and here is the response: ${JSON.stringify(
+      responseData,
+      null,
+      2
+    )}`
   );
-  return nonNull_properties;
-}
+})();
 
-// Function to save the properties to Firestore
-async function saveToFirestore(nonNull_properties) {
-  let companyName = nonNull_properties.name;
-  // replace the spaces with "_"
-  companyName = companyName.replace(/\s/g, "_");
-  // Keep only the first 2 words
-  companyName = companyName.split("_").slice(0, 2).join("_");
-
-  let docID = companyName + "_" + nonNull_properties.companyId;
-  const db = admin.firestore();
-  const docRef = db.collection(firtestoreCollection).doc(`${docID}`);
-  docRef.set(nonNull_properties);
-}
+// ----------------------- End of Main ----------------------------
